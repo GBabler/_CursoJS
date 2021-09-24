ba285c7bd1543fe4cfc47b2af42567ee9a4907a5
Gabriel Babler <gabriel.babler1@hotmail.com>
2021-09-24 15:30:46 -0300

    Add files via upload

---
+++ _exericios/contaBancaria/js/script.js
@@ -0,0 +1,14 @@
+var saldo =  document.querySelector('div#saldoAtual')
+saldo =  1500
+
+function fncExtrato(){
+    saldo.innerHTML = `Seu saldo atual é de ${saldo}`
+}
+
+function fncRetirar(){
+
+}
+
+function fncDepositar(){
+
+}
